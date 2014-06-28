1df4f82cea2c8dd6cad8108373ffa06b369398aa
KingCreeperEli <ess0523@gmail.com>
2014-06-28 12:22:59 -0400

    Fixed WineBlock

---
+++ src/main/java/FaithCraft2/Common/common/blocks/WineBlock.java
@@ -0,0 +1,50 @@
+package FaithCraft2.Common.common.blocks;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.BlockFluidClassic;
+import net.minecraftforge.fluids.Fluid;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import FaithCraft2.Common.common.FaithCraft2;
+
+public class WineBlock extends BlockFluidClassic{
+	@SideOnly(Side.CLIENT)
+    protected IIcon stillIcon;
+    @SideOnly(Side.CLIENT)
+    protected IIcon flowingIcon;
+    
+    public WineBlock(Fluid fluid, Material material) {
+            super(fluid, material);
+            
+            this.setCreativeTab(FaithCraft2.FaithCraft2Tab);
+    }
+
+	@Override
+    public IIcon getIcon(int side, int meta) {
+            return (side == 0 || side == 1)? stillIcon : flowingIcon;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerBlockIcons(IIconRegister register) {
+            stillIcon = register.registerIcon("FaithCraft2:WineStill");
+            flowingIcon = register.registerIcon("FaithCraft2:WineFlow");
+    }
+    
+    @Override
+    public boolean canDisplace(IBlockAccess world, int x, int y, int z) {
+            if (world.getBlock(x,  y,  z).getMaterial().isLiquid()) return false;
+            return super.canDisplace(world, x, y, z);
+    }
+    
+    @Override
+    public boolean displaceIfPossible(World world, int x, int y, int z) {
+            if (world.getBlock(x,  y,  z).getMaterial().isLiquid()) return false;
+            return super.displaceIfPossible(world, x, y, z);
+    }
+}
