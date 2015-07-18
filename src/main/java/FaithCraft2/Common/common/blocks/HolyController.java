de4bc31926a800019c5a263a9b4476f543a84c22
KingCreeperEli <ess0523@gmail.com>
2015-07-18 13:13:51 -0400

    Changed the HolyController Texture and added particles.

--- src/main/java/FaithCraft2/Common/common/blocks/HolyController.java
+++ src/main/java/FaithCraft2/Common/common/blocks/HolyController.java
@@ -1,6 +1,7 @@
 package FaithCraft2.Common.common.blocks;
 
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -14,6 +15,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -34,11 +36,70 @@ public class HolyController extends Block{
 		this.setLightLevel(1.0F);
 		this.setDefaultState(this.blockState.getBaseState().withProperty(VARIANT, HolyController.EnumType.OFF));
 		this.damageDropped(this.getDefaultState());
+		this.setTickRandomly(true);
 	}
 	
 	public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
 		this.isHolyForge = false;
 	}
+	
+	@SideOnly(Side.CLIENT)
+    public void randomDisplayTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        if (this.isHolyForge)
+        {
+            this.spawnParticles(worldIn, pos);
+        }
+    }
+
+    private void spawnParticles(World worldIn, BlockPos pos)
+    {
+        Random random = worldIn.rand;
+        double d0 = 0.0625D;
+
+        for (int i = 0; i < 6; ++i)
+        {
+            double d1 = (double)((float)pos.getX() + random.nextFloat());
+            double d2 = (double)((float)pos.getY() + random.nextFloat());
+            double d3 = (double)((float)pos.getZ() + random.nextFloat());
+
+            if (i == 0 && !worldIn.getBlockState(pos.up()).getBlock().isOpaqueCube())
+            {
+                d2 = (double)pos.getY() + d0 + 1.0D;
+            }
+
+            if (i == 1 && !worldIn.getBlockState(pos.down()).getBlock().isOpaqueCube())
+            {
+                d2 = (double)pos.getY() - d0;
+            }
+
+            if (i == 2 && !worldIn.getBlockState(pos.south()).getBlock().isOpaqueCube())
+            {
+                d3 = (double)pos.getZ() + d0 + 1.0D;
+            }
+
+            if (i == 3 && !worldIn.getBlockState(pos.north()).getBlock().isOpaqueCube())
+            {
+                d3 = (double)pos.getZ() - d0;
+            }
+
+            if (i == 4 && !worldIn.getBlockState(pos.east()).getBlock().isOpaqueCube())
+            {
+                d1 = (double)pos.getX() + d0 + 1.0D;
+            }
+
+            if (i == 5 && !worldIn.getBlockState(pos.west()).getBlock().isOpaqueCube())
+            {
+                d1 = (double)pos.getX() - d0;
+            }
+
+            if (d1 < (double)pos.getX() || d1 > (double)(pos.getX() + 1) || d2 < 0.0D || d2 > (double)(pos.getY() + 1) || d3 < (double)pos.getZ() || d3 > (double)(pos.getZ() + 1))
+            {
+                worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, d1, d2, d3, 0.0D, 0.0D, 0.0D, new int[0]);
+                worldIn.spawnParticle(EnumParticleTypes.FLAME, d1, d2, d3, 0.0D, 0.0D, 0.0D, new int[0]);
+            }
+        }
+    }
 	
 	@Override
 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ){
